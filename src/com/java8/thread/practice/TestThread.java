d8e8163dfece43b084d4da29ebe27d7adaba7acd
bbolke <bbolke@BBOLKE2L2.ptcnet.ptc.com>
2019-11-25 17:12:16 +0530

    added new problems

--- src/com/java8/thread/practice/TestThread.java
+++ src/com/java8/thread/practice/TestThread.java
@@ -1,11 +1,14 @@
 package com.java8.thread.practice;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,7 +17,82 @@ public class TestThread {
 		// useMyThread();
 		// useMyRunnable();
 		// useExecutors();
-		useMyCallable();
+		//useMyCallable();
+		practiceThread1();
+	}
+
+	private static void practiceThread1() {
+//		Runnable runnable = () -> {
+//			try {
+//			String name = Thread.currentThread().getName();
+//			System.out.println("Foo " + name);
+//			
+//				TimeUnit.SECONDS.sleep(1);
+//			
+//			System.out.println("Bar " + name);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		};
+		
+//		Thread thread = new Thread(runnable);
+//		thread.start();
+		
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		/*
+		 * executor.submit(()-> { String threadName = Thread.currentThread().getName();
+		 * System.out.println("Hello " + threadName); });
+		 */
+		//executor.shutdownNow();
+		
+		Callable<String> task = () -> {
+			TimeUnit.SECONDS.sleep(5);
+			return "Bramha";
+		};
+		
+//		Future<String> future = executor.submit(task);
+//		//executor.shutdownNow();
+//		
+//		try {
+//			System.out.println(future.isDone());
+//			System.out.println(future.get(3,TimeUnit.SECONDS));
+//			System.out.println(future.isDone());
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (ExecutionException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (TimeoutException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		executor = Executors.newWorkStealingPool();
+		
+		List<Callable<String>> callbles = Arrays.asList(
+				()  -> "Task1",
+				()  -> "Task2",
+				()  -> "Task3");
+		
+		try {
+			executor.invokeAll(callbles).parallelStream().map(future -> {
+				try {
+					return future.get();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ExecutionException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return null;
+			}).forEach(System.out::println);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	private static void useMyCallable() {
